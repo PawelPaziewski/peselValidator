f7b0e36b59469ce5f60635301a50024916800ac8
PawelPaziewski <lrpapawel@gmail.com>
2020-03-29 22:30:14 +0200

    Make PeselValidator implements PeselValidatorInterface

--- src/Pesel/PeselValidator.java
+++ src/Pesel/PeselValidator.java
@@ -1,9 +1,11 @@
+package Pesel;
+
 import java.util.LinkedList;
 
-public class PeselValidator {
+public class PeselValidator{
     private static final int[] checkweights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
 
-    private static boolean correctPesel(LinkedList<Integer> peselList) {
+    static boolean validate(LinkedList<Integer> peselList) {
         int controlNumber = 0;
         for (int i = 0; i < 10; i++) {
             controlNumber += (peselList.get(i) * checkweights[i]) % 10;
