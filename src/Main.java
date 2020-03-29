a4a736cd0c56d960c6b5dc1d26df58593a0762ad
PawelPaziewski <lrpapawel@gmail.com>
2020-03-29 19:36:49 +0200

    Add a pesel reader

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        PeselReader reader=new PeselReader();
+        System.out.println(reader.getPesel());
+    }
+}
---
+++ src/PeselReader.java
@@ -0,0 +1,49 @@
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class PeselReader {
+
+    private long readedPesel;
+    private LinkedList<Integer> pesel;
+
+    public LinkedList<Integer> getPesel() {
+        return pesel;
+    }
+
+    public PeselReader() {
+        this.readedPesel = readPesel();
+        this.pesel = convertToIntArray(readedPesel);
+    }
+
+    private long readPesel() {
+        Scanner scanner = new Scanner(System.in);
+        long pesel = 0L;
+        boolean readed;
+        do {
+            readed = true;
+            System.out.println("Give me your ID number: ");
+            try {
+                pesel = scanner.nextLong();
+            } catch (InputMismatchException e) {
+                System.out.println("Bad ID number!!");
+                readed = false;
+                scanner.nextLine();
+            }
+        } while (!readed || pesel > 99999999999L);
+        return pesel;
+    }
+
+    private LinkedList<Integer> convertToIntArray(long pesel) {
+        LinkedList<Integer> peselArray = new LinkedList<>();
+        while (pesel != 0) {
+            peselArray.add((int) (pesel % 10));
+            pesel /= 10;
+        }
+        if (peselArray.size() != 11)//leading 0
+            peselArray.add(0);
+        Collections.reverse(peselArray);
+        return peselArray;
+    }
+}
